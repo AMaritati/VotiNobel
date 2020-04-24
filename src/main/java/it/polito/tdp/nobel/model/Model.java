5a6147b6834c52da03bee80450de15c968e60bcc
Administrator <Administrator@BATAR02M002.rete.poste>
2020-04-24 18:34:01 +0200

    ok

--- src/main/java/it/polito/tdp/nobel/model/Model.java
+++ src/main/java/it/polito/tdp/nobel/model/Model.java
@@ -1,15 +1,81 @@
 package it.polito.tdp.nobel.model;
 
+import java.util.HashSet;
+import java.util.List;
 import java.util.Set;
+
+import it.polito.tdp.nobel.db.EsameDAO;
 
 public class Model {
 
+	private List<Esame> esami;
+	private Set<Esame> soluzione = null;
+	private double bestMedia;
+
 	
+	public Model() {
+		EsameDAO e = new EsameDAO();
+		esami = e.getTuttiEsami();
+		
+	}
 	public Set<Esame> calcolaSottoinsiemeEsami(int numeroCrediti) {
+		Set<Esame> parziale = new HashSet<>();
 		
-		System.out.println("TODO!");
 		
-		return null;
+		bestMedia = 0.0;
+		soluzione = new HashSet<>();
+		
+		
+		cerca(parziale,0,numeroCrediti);
+		
+		
+		return soluzione;
 	}
 
+	private void cerca (Set<Esame> parziale, int livello, int m) {
+		int somma = sommaCrediti(parziale);
+		
+		// casi terminali
+		if (somma > m) {
+			return;
+		}
+		
+		if (somma == m) {
+			//controllo la media
+			double media = mediaCrediti(parziale);
+			if (media > bestMedia) {
+				bestMedia = media;
+				soluzione = new HashSet<>(parziale);
+			}
+			return;
+		}
+		
+		if (livello == esami.size()) {
+			return;
+		}
+		
+		parziale.add(esami.get(livello));
+		cerca(parziale,livello+1,m);
+		parziale.remove(esami.get(livello));
+	
+		cerca (parziale, livello+1, m);
+		
+	}
+	private double mediaCrediti(Set<Esame> parziale) {
+		int somma = 0;
+		int crediti = 0;
+		
+		for (Esame e : parziale) {
+			crediti += e.getCrediti();
+			somma += (e.getVoto() * e.getCrediti());
+		}
+		return somma/crediti;
+	}
+	private int sommaCrediti(Set<Esame> parziale) {
+		int somma=0;
+		for(Esame e : esami) {
+			somma+=e.getCrediti();
+		}
+		return somma;
+	}
 }
--- src/main/java/it/polito/tdp/nobel/model/TestModel.java
+++ src/main/java/it/polito/tdp/nobel/model/TestModel.java
@@ -1,12 +1,19 @@
 package it.polito.tdp.nobel.model;
 
+import it.polito.tdp.nobel.db.EsameDAO;
+
 public class TestModel {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
 		Model model = new Model();
-		model.calcolaSottoinsiemeEsami(20);
+		EsameDAO E = new EsameDAO();
+		
+		System.out.println(E.getTuttiEsami());
+	
+		model.calcolaSottoinsiemeEsami(50);
+		
 		
 	}
 
